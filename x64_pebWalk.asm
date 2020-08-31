07d0602f974cf4bfe6242598b1da3b7b52e0c8df
Connor McGarr <45445720+connormcgarr@users.noreply.github.com>
2020-08-31 15:31:36 -0500

    Update x64_pebWalk.asm

--- x64_pebWalk.asm
+++ x64_pebWalk.asm
@@ -13,12 +13,13 @@ _start PROC
 	mov dl, 4bh					; "K" (Change this to the first letter of the DLL you want to find)
 	call _walkPEB
 _start ENDP
+
 ; Walk loaded modules via InLoadOrderModuleList (doubly linked list where each list points to an LDR_DATA_TABLE_ENTRY)
 ; We technically "skip" the first loaded module, which is the current process (the process in which this assembly is running in) in order to start the loop
 _walkPEB PROC
 	mov rsi, [rsi]				; Dereference first member of LDR_DATA_TABLE_ENTRY structure, InLoadOrderLinks, to get next module in the doubly linked list
 	mov rax, [rsi+30h]			; Save base address of current module via LDR_DATA_TABLE_ENTRY
-	mov rdi, [rsi+58h+8h]		; Store name of module into RDI (_UNICODE_STRING). First 8 bytes of BaseDLLName consists of Length and MaximumLength members
+	mov rdi, [rsi+58h+8h]			; Store name of module into RDI (_UNICODE_STRING). First 8 bytes of BaseDLLName consists of Length and MaximumLength members
 	cmp [rdi+12*2], cx			; KERNEL32.DLL is 12 bytes (x2 for unicode). Does the 24th byte, which should be a NULL terminator, equal 00?
 	jne _walkPEB				; Didn't equal 00? Keep looping
 	cmp [rdi], dl				; Have we located the desired module?
